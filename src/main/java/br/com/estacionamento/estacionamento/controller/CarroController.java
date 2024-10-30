ad42dd668573a3e08253c95c0add458259bcb453
Gabriel Pierri <gabrielpiierri.nunes@gmail.com>
2024-10-29 21:25:22 -0300

    Foi criado o CRUD para Carro

--- src/main/java/br/com/estacionamento/estacionamento/controller/CarroController.java
+++ src/main/java/br/com/estacionamento/estacionamento/controller/CarroController.java
@@ -4,10 +4,9 @@ import br.com.estacionamento.estacionamento.dto.CarroDTO;
 import br.com.estacionamento.estacionamento.model.Carro;
 import br.com.estacionamento.estacionamento.service.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/carro")
@@ -20,5 +19,25 @@ public class CarroController {
     public CarroDTO cadastrarCarro(@RequestBody Carro carro) {
         Carro retorno = carroService.cadastrarCarro(carro);
         return new CarroDTO(retorno);
+    }
+
+    @GetMapping()
+    public List<Carro> buscarTodosCarros() {
+        return carroService.buscarTodosCarros();
+    }
+
+    @GetMapping("/{id}")
+    public Carro buscarCarroPorId(@PathVariable Long id) {
+        return carroService.buscarCarroPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Carro editarCarro(@PathVariable Long id, @RequestBody Carro carro) {
+        return carroService.editarCarro(id, carro);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarCarro(@PathVariable Long id) {
+        carroService.deletarCarro(id);
     }
 }
--- src/main/java/br/com/estacionamento/estacionamento/service/CarroService.java
+++ src/main/java/br/com/estacionamento/estacionamento/service/CarroService.java
@@ -5,6 +5,8 @@ import br.com.estacionamento.estacionamento.repository.CarroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CarroService {
 
@@ -13,5 +15,30 @@ public class CarroService {
 
     public Carro cadastrarCarro(Carro carro) {
         return carroRepository.save(carro);
+    }
+
+    public List<Carro> buscarTodosCarros() {
+        return carroRepository.findAll();
+    }
+
+    public Carro buscarCarroPorId(Long id) {
+        return carroRepository.findById(id).orElseThrow(() -> new RuntimeException("Carro não encontrado!"));
+    }
+
+    public Carro editarCarro(Long id, Carro carro) {
+        if (!id.equals(carro.getId())) {
+            throw new RuntimeException("O id não corresponde a esse carro!");
+        }
+        Carro carroEditado = buscarCarroPorId(id);
+        carroEditado.setMarca(carro.getMarca());
+        carroEditado.setPlaca(carro.getPlaca());
+        carroEditado.setModelo(carro.getModelo());
+        carroRepository.save(carroEditado);
+        return carroEditado;
+    }
+
+    public void deletarCarro(Long id) {
+        Carro carro = buscarCarroPorId(id);
+        carroRepository.delete(carro);
     }
 }
